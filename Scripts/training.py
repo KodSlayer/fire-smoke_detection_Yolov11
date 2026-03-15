c43611ea4d3f620d71c24192f79f8688326cc57f
KodSlayer <yashateshi08@gmail.com>
2026-03-15 19:22:42 +0530

    Added the script for training yolov11 model with dataset

---
+++ Scripts/training.py
@@ -0,0 +1,27 @@
+# This is the script to train the model after unzipping 
+
+
+import os
+from ultralytics import YOLO
+
+# Verification of dataset path
+data_path = "/content/Fire-Smoke-Detection-Yolov11-2/data.yaml"
+if not os.path.exists(data_path):
+    print(f"❌ Warning: {data_path} not found. Check if the extraction was successful.")
+else:
+    model = YOLO("yolo11n.pt")
+
+    results = model.train(
+        data=data_path,
+        epochs=30,
+        imgsz=640,
+        batch=16,
+        name="fire_smoke_yolo11",
+        patience=10,
+        device=0,
+        workers=4,
+        save=True,
+        plots=True
+    )
+
+    print("✅ Training complete!")
